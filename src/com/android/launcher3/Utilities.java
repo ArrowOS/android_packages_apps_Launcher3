c73d53652c03a316c615e29533bda25bf0515ce6
Joey <joey@lineageos.org>
2018-02-04 11:36:32 +0100

    Launcher3: allow disabling workspace edit
    [BadDaemon: Adapt for S changes]
    
    Change-Id: I503e19cbc512eac0e4a8c8bccc16a6ccc0e805da
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -938,4 +938,9 @@ public final class Utilities {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
+
+    public static boolean isWorkspaceEditAllowed(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
+    }
 }
