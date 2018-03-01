3d04a4d10d2f711a8eaace1b9851dd9c961190aa
Alexey Derendyaev <alexxxdev@yandex.ru>
2018-03-01 23:15:36 +0700

    Option to hide/show labels on desktop & app-drawer
    AICP Edit: adapted to 10
    TODO: apply to folder labels (WorkspaceLayoutManager)
    Change-Id: Ie790dee6a31e0d1e15f1c7a05f936815b3388fb2

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -166,6 +166,11 @@ public final class Utilities {
     public static final String GRID_COLUMNS = "pref_grid_columns";
     public static final String GRID_ROWS = "pref_grid_rows";
     public static final String HOTSEAT_ICONS = "pref_hotseat_icons";
+    public static final String DESKTOP_SHOW_LABEL = "pref_desktop_show_label";
+    public static final String ALLAPPS_SHOW_LABEL = "pref_allapps_show_label";
+
+    public static final String PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+
     /**
      * An {@link Executor} to be used with async task with no limit on the queue size.
      */
@@ -213,6 +218,14 @@ public final class Utilities {
         } catch (Exception e) {
             return preferenceFallback;
         }
+    }
+
+    public static boolean showDesktopLabel(Context context) {
+        return getPrefs(context).getBoolean(DESKTOP_SHOW_LABEL, true);
+    }
+
+    public static boolean showAllAppsLabel(Context context) {
+         return getPrefs(context).getBoolean(ALLAPPS_SHOW_LABEL, true);
     }
 
     /**
