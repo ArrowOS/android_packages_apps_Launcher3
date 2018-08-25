717aa1cb61db2a215b87a08af4706d84db04f58f
maxwen <max.weninger@gmail.com>
2018-08-25 11:51:27 +0200

    Launcher3: add theme style switch
    additional to automatic wallpaper based allow
    setting dedicated light or dark style
    
    Change-Id: I70435beda7d4b5a4e2b84fe33bb277c4e3356194

--- src/com/android/launcher3/graphics/IconShapeOverride.java
+++ src/com/android/launcher3/graphics/IconShapeOverride.java
@@ -61,6 +61,12 @@ public class IconShapeOverride {
         if (!Utilities.ATLEAST_OREO) {
             return false;
         }
+        // Only supported when developer settings is enabled
+        /*if (Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 1) {
+            return false;
+        }*/
+
 
         try {
             if (getSystemResField().get(null) != Resources.getSystem()) {
