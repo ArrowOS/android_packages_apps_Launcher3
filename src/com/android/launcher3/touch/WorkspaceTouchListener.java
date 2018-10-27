82c217cf428f5d958be316a832e3d55a2bfa55c2
ezio84 <brabus84@gmail.com>
2018-10-27 23:40:21 +0200

    Add double tap to sleep gesture
    Change-Id: Icf2cc27a70b904904baab10ca83d17e6ea9bc06e
    todo: use WorkspaceTouchListener onTouch to handle all touch types
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -82,7 +82,7 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     @Override
     public boolean onTouch(View view, MotionEvent ev) {
         mGestureDetector.onTouchEvent(ev);
-
+        mWorkspace.checkDoubleTap(ev);
         int action = ev.getActionMasked();
         if (action == ACTION_DOWN) {
             // Check if we can handle long press.
