66f4e6ac4e69379b2e03a7613d95263968759904
ElDainosor <eldainosor@gmail.com>
2018-11-01 21:22:10 -0500

    Launcher3: Set a toggle to show Search bar on first screen
    Instead of hardcoding it on the FeatureFlags, let's make it
    toggleable as any other option would do
    
    Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/qsb/QsbContainerView.java
+++ src/com/android/launcher3/qsb/QsbContainerView.java
@@ -287,7 +287,7 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return FeatureFlags.QSB_ON_FIRST_SCREEN;
+            return Utilities.showQSB(getContext());
         }
 
         protected Bundle createBindOptions() {
