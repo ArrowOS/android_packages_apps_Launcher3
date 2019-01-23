dc5d7396d03e7a338287c175b480d3f0611e1bf8
Joey <joey@lineageos.org>
2019-01-23 17:49:25 +0100

    Launcher3: implement hidden & protected apps
    @neobuddy89: Adapted for A12.
    
    * Launcher3: implement hidden apps
    * Launcher3: Implement protected apps
    
    Squashed with:
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sat Nov 2 15:23:42 2019 +0100
    
        Switch to BiometricPrompt
    
        Change-Id: Id6700c81aadb64434aecf1e54ae8b0f01d8143fb
    
    Author: Joey <joey@lineageos.org>
    Date:   Fri Feb 7 22:18:51 2020 +0100
    
        Launcher3: fix protected apps behaviour
    
        If no secure lockscreen is set we should not show
        the app list
    
        Also make icons' touchable area larger
    
        Change-Id: Id398e98cb4d8fdae169aae194211b98e6441b4cf
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Sat Apr 4 10:57:37 2020 +0200
    
        Launcher3: only allow hidden apps when no secure keyguard is set
    
        Change-Id: I670a61ff3fee4d7c7d9623ce8810790f1b25224a
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Wed Aug 5 13:23:03 2020 +0200
    
        Launcher3: improve protected app names style
    
        Change-Id: I09e9995f0eb34de23353166e302481d064fb3058
        Signed-off-by: Joey <joey@lineageos.org>
    
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: CLHEX <clhexftw@protonmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -55,7 +55,8 @@ import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.model.WidgetsModel;
 import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.uioverrides.plugins.PluginManagerWrapper;
-
+import com.android.launcher3.lineage.LineageUtils;
+import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import java.util.Collections;
 import java.util.List;
 
@@ -80,7 +81,8 @@ public class SettingsActivity extends FragmentActivity
     public static final String EXTRA_SHOW_FRAGMENT_ARGS = ":settings:show_fragment_args";
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
-
+    public static final String KEY_TRUST_APPS = "pref_trust_apps";
+    
     @VisibleForTesting
     static final String EXTRA_FRAGMENT = ":settings:fragment";
     @VisibleForTesting
@@ -287,6 +289,18 @@ public class SettingsActivity extends FragmentActivity
                 case DEVELOPER_OPTIONS_KEY:
                     mDeveloperOptionPref = preference;
                     return updateDeveloperOption();
+
+
+                case KEY_TRUST_APPS:
+                    preference.setOnPreferenceClickListener(p -> {
+                        LineageUtils.showLockScreen(getActivity(),
+                                getString(R.string.trust_apps_manager_name), () -> {
+                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                            startActivity(intent);
+                        });
+                        return true;
+                    });
+                    return true;
 
                 case KEY_ENABLE_MINUS_ONE:
                     mShowGoogleAppPref = preference;
