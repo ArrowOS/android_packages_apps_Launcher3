f3ae9edd0dd639dfd7257376fc8d48b014f18a00
Joey <joey@lineageos.org>
2019-01-23 17:49:25 +0100

    Launcher: implement hidden & protected apps
    * Trebuchet: implement hidden apps
    * Trebuchet: Implement protected apps
    
    @DennySPb: port to AOSP launcher
    
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -55,6 +55,7 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.app.ActivityOptions;
+import android.app.KeyguardManager;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetManager;
 import android.content.ActivityNotFoundException;
@@ -342,6 +343,11 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     private boolean mTouchInProgress;
 
     private SafeCloseable mUserChangedCallbackCloseable;
+
+    private static final int REQUEST_AUTH_CODE = 93;
+    private View mAuthView;
+    private ItemInfo mAuthInfo;
+    private String mAuthContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -699,6 +705,13 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
                 mStateManager.goToState(NORMAL, SPRING_LOADED_EXIT_DELAY);
             }
         };
+
+        if (requestCode == REQUEST_AUTH_CODE) {
+            if (resultCode == RESULT_OK) {
+                startActivitySafely(mAuthView, requestArgs.getPendingIntent(), mAuthInfo, mAuthContainer);
+            }
+            return;
+        }
 
         if (requestCode == REQUEST_BIND_APPWIDGET) {
             // This is called only if the user did not previously have permissions to bind widgets
@@ -1074,6 +1087,31 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         if (!mDeferOverlayCallbacks) {
             mOverlayManager.onActivityPaused(this);
         }
+    }
+
+    public void startActivitySafelyAuth(View v, Intent intent, ItemInfo item,
+            String sourceContainer) {
+        KeyguardManager manager = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ?
+                getSystemService(KeyguardManager.class) :
+                (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        if (manager == null) {
+            throw new NullPointerException("No KeyguardManager found!");
+        }
+
+        String title = getString(R.string.trust_apps_manager_name);
+        String message = getString(R.string.trust_apps_auth_open_app, item.title);
+        Intent kmIntent = manager.createConfirmDeviceCredentialIntent(title, message);
+
+        if (kmIntent != null) {
+            mAuthView = v;
+            mAuthInfo = item;
+            mAuthContainer = sourceContainer;
+            setWaitingForResult(PendingRequestArgs.forIntent(REQUEST_AUTH_CODE, intent, item));
+            startActivityForResult(kmIntent, REQUEST_AUTH_CODE);
+            return;
+        }
+
+        startActivitySafely(v, intent, item, sourceContainer);
     }
 
     class LauncherOverlayCallbacksImpl implements LauncherOverlayCallbacks {
