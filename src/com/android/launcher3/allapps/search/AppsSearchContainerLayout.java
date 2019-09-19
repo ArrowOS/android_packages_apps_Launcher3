cf6bc2ff29170a5143acd714d174def7e83b0c89
paphonb <paphonb@gmail.com>
2019-09-19 23:14:18 +0700

    Fix launcher preview rendering
    Change-Id: Ic7376140656e3dc861679fedf8f14862fc90aca8

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -45,6 +45,7 @@ import com.android.launcher3.allapps.AlphabeticalAppsList;
 import com.android.launcher3.allapps.SearchUiManager;
 import com.android.launcher3.anim.PropertySetter;
 import com.android.launcher3.util.ComponentKey;
+import com.android.launcher3.views.ActivityContext;
 
 import java.util.ArrayList;
 
@@ -56,6 +57,8 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         AllAppsStore.OnUpdateListener, Insettable {
 
     private final BaseDraggingActivity mLauncher;
+
+    private final ActivityContext mActivity;
     private final AllAppsSearchBarController mSearchBarController;
     private final SpannableStringBuilder mSearchQueryBuilder;
 
@@ -77,6 +80,8 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         super(context, attrs, defStyleAttr);
 
         mLauncher = BaseDraggingActivity.fromContext(context);
+        mActivity = ActivityContext.lookupContext(context);
+
         mSearchBarController = new AllAppsSearchBarController();
 
         mSearchQueryBuilder = new SpannableStringBuilder();
@@ -102,10 +107,11 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         // Update the width to match the grid padding
-        DeviceProfile dp = mLauncher.getDeviceProfile();
+        DeviceProfile dp = mActivity.getDeviceProfile();
         int myRequestedWidth = getSize(widthMeasureSpec);
-        int rowWidth = myRequestedWidth - mAppsView.getActiveRecyclerView().getPaddingLeft()
-                - mAppsView.getActiveRecyclerView().getPaddingRight();
+        int leftRightPadding = dp.desiredWorkspaceLeftRightMarginPx
+                + dp.cellLayoutPaddingLeftRightPx;
+        int rowWidth = myRequestedWidth - leftRightPadding * 2;
 
         int cellWidth = DeviceProfile.calculateCellWidth(rowWidth, dp.inv.numHotseatIcons);
         int iconVisibleSize = Math.round(ICON_VISIBLE_AREA_FACTOR * dp.iconSizePx);
@@ -202,7 +208,7 @@ public class AppsSearchContainerLayout extends ExtendedEditText
 
     @Override
     public float getScrollRangeDelta(Rect insets) {
-        if (mLauncher.getDeviceProfile().isVerticalBarLayout()) {
+        if (mActivity.getDeviceProfile().isVerticalBarLayout()) {
             return 0;
         } else {
             return insets.bottom + insets.top;
