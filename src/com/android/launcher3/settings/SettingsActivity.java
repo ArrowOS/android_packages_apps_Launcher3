576f407cc49d346106075d85ebdf94aa6f76c367
hundeva <hundeva@gmail.com>
2019-10-12 07:33:14 +0000

    Launcher3: Add restart action for toggling settings
    idoybh: make it work with gestures, improve committed
    
    Change-Id: I554b5203496cc1fe1129940520de7c705d69813c
    Co-authored-by: Ido Ben-hur <idoybh2@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -50,6 +50,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.InvariantDeviceProfile;
+import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
@@ -187,6 +188,7 @@ public class SettingsActivity extends FragmentActivity
         }
         return super.onOptionsItemSelected(item);
     }
+
 
     /**
      * This fragment shows the launcher preferences.
@@ -340,6 +342,12 @@ public class SettingsActivity extends FragmentActivity
                 }
             }
             updateIsGoogleAppEnabled();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            LauncherAppState.getInstanceNoCreate().checkIfRestartNeeded();
         }
 
         private PreferenceHighlighter createHighlighter() {
