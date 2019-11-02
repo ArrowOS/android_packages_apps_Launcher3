2f56ef04248531e16f18c6e09d21ea2d2d4ff472
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-02 15:23:42 +0100

    Protected Apps: Switch to BiometricPrompt
    Change-Id: Id6700c81aadb64434aecf1e54ae8b0f01d8143fb
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -278,8 +278,11 @@ public class SettingsActivity extends Activity
                             PluginManagerWrapper.hasPlugins(getContext());
                 case KEY_TRUST_APPS:
                     preference.setOnPreferenceClickListener(p -> {
-                        Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
-                        startActivity(intent);
+                        Utilities.showLockScreen(getActivity(),
+                                getString(R.string.trust_apps_manager_name), () -> {
+                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                            startActivity(intent);
+                        });
                         return true;
                     });
                     return true;
