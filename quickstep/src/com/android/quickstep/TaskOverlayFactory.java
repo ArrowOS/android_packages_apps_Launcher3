c1ea72029c0b4c8bd706ca40bf084817fa7e4491
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: Add uninstall button to system shortcuts
    Change-Id: I005d676d9a98f65296c330e5e13fd0d849df6fe5
    [ghostrider-reborn: Adapt to Sv2]
    [jhonboy121]: adapt to A13
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I2dc5b70aca86e4e893ea59a3455137a0ac8e86e1

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -172,6 +172,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             TaskShortcutFactory.APP_INFO,
             TaskShortcutFactory.KILL_APP,
             TaskShortcutFactory.SPLIT_SCREEN,
+            TaskShortcutFactory.UNINSTALL,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
