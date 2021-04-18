dbff6c0fa928f0609e8f490b08f460b1b7b28bbc
palaych <palaychm@yandex.ru>
2021-04-18 13:44:46 +0000

    Launcher3: Return hidden apps feature
    commit b8258b9f66d9aba24cb3a09511a74f7b935e2a30 (HEAD)
    Author: DennySPb <dennyspb@gmail.com>
    Date:   Mon Mar 29 15:59:24 2021 +0300
    
        Launcher: Fix SQL statement after Protected apps removal
    
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Change-Id: I11e8fb2b013f18922d3c5119cdf108956e9360c9
    
    commit c8e31295852722b1160ea5048df307c971ec330b
    Author: DennySPb <dennyspb@gmail.com>
    Date:   Tue Mar 23 14:47:24 2021 +0300
    
        Launcher: Restore biometric prompt for Hidden Apps
    
        this was accidentally deleted with Protected apps removal
    
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Change-Id: I279c3981b796b4f2b9de375d68bf3a5539940418
    
    commit f7a688b0a3799e95df550fd3ad346df3ee14e878
    Author: DennySPb <dennyspb@gmail.com>
    Date:   Tue Mar 23 12:30:12 2021 +0300
    
        Launcher: Allow to tap at whole item in Hidden Apps
    
        since Protected apps were removed, we could be more user friendly
        and should allow to change Hidden apps state by tapping on app label and icon
    
        Change-Id: Ia69ec502141d6969241be972be5dc6f2a7f17079
        Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    commit 068e6aa769444ad86c66a699be67d65d54b0daa7
    Author: DennySPb <dennyspb@gmail.com>
    Date:   Mon Mar 22 16:13:47 2021 +0300
    
        Launcher: Remove Protected apps in favour of system AppLock
    
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Change-Id: Ie785da5f44389a87cb2a8a42a8e40f817fc72228
    
    commit 86d8ac570fe17e2b0f94a6705374c3144a347ba8
    Author: palaych <palaychm@yandex.ru>
    Date:   Sun Apr 18 13:38:47 2021 +0000
    
        Revert "Launcher3: Drop hidden & protected apps"
    
        This reverts commit 0a842a2be6a95fff51c8650fa1bc5a149e14c176.
    
    Change-Id: Ib41a9ec3bd0e06167fd5cf85566e88ca7f2de7a3

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1083,6 +1083,13 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         }
     }
 
+    public void startActivitySafelyAuth(View v, Intent intent, ItemInfo item,
+            String sourceContainer) {
+        Utilities.showLockScreen(this, getString(R.string.trust_apps_manager_name), () -> {
+            startActivitySafely(v, intent, item, sourceContainer);
+        });
+    }
+
     class LauncherOverlayCallbacksImpl implements LauncherOverlayCallbacks {
 
         public void onScrollChanged(float progress) {
