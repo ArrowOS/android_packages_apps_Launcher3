653167bf4aa95710c2f7b558db48444b6346d6da
Pranav Vashi <neobuddy89@gmail.com>
2022-01-11 11:33:42 +0530

    Launcher3: Fix icon palette crashes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I7d641ec3e2cb2f8ad11a16df8143c32a6deb2d59

--- src/com/android/launcher3/graphics/IconPalette.java
+++ src/com/android/launcher3/graphics/IconPalette.java
@@ -100,8 +100,8 @@ public class IconPalette {
     /** For debugging. This was copied from com.android.internal.util.NotificationColorUtil. */
     private static String contrastChange(int colorOld, int colorNew, int bg) {
         return String.format("from %.2f:1 to %.2f:1",
-                ColorUtils.calculateContrast(colorOld, bg),
-                ColorUtils.calculateContrast(colorNew, bg));
+                calculateContrast(colorOld, bg),
+                calculateContrast(colorNew, bg));
     }
 
     /**
@@ -133,7 +133,7 @@ public class IconPalette {
      * This was copied from com.android.internal.util.NotificationColorUtil.
      */
     private static int findContrastColor(int fg, int bg, double minRatio) {
-        if (ColorUtils.calculateContrast(fg, bg) >= minRatio) {
+        if (calculateContrast(fg, bg) >= minRatio) {
             return fg;
         }
 
@@ -149,13 +149,22 @@ public class IconPalette {
         for (int i = 0; i < 15 && high - low > 0.00001; i++) {
             final double l = (low + high) / 2;
             fg = ColorUtils.LABToColor(l, a, b);
-            if (ColorUtils.calculateContrast(fg, bg) > minRatio) {
+            if (calculateContrast(fg, bg) > minRatio) {
                 if (isBgDark) high = l; else low = l;
             } else {
                 if (isBgDark) low = l; else high = l;
             }
         }
         return ColorUtils.LABToColor(low, a, b);
+    }
+
+    private static double calculateContrast(int fg, int bg) {
+        bg = setAlphaComponent(bg, 255);
+        return ColorUtils.calculateContrast(fg, bg);
+    }
+
+    private static int setAlphaComponent(int color, int alpha) {
+        return (color & 0x00ffffff) | (alpha << 24);
     }
 
     public static int getMutedColor(int color, float whiteScrimAlpha) {
