0c0b5f3f5d59e97350438f7fa63f538e9571240e
jhonboy121 <alfredmathew05@gmail.com>
2022-10-03 21:18:26 +0530

    AssistantIconView|QsbLayout: fixup and improve code
    * Check if search package is null so as to prevent crash on click
    * Keep the intents as static variables
    * Move assistant icon click logic to QsbLayout since it avoids repetition of code
    * Finalise class and variables, drop unnecessary imports and instance variables
    * Added more null checks coz jeva bess
    * use the new pm API for resolveActivity
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ie2df0917f944b699b746dd18b7e849a3c5c85c2b

--- src/com/android/launcher3/qsb/QsbLayout.java
+++ src/com/android/launcher3/qsb/QsbLayout.java
@@ -3,56 +3,87 @@ package com.android.launcher3.qsb;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.ResolveInfoFlags;
+import android.content.pm.ResolveInfo;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import androidx.core.view.ViewCompat;
-import com.android.launcher3.BaseActivity;
+
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.R;
 import com.android.launcher3.qsb.QsbContainerView;
 import com.android.launcher3.views.ActivityContext;
-import android.view.View;
 
-public class QsbLayout extends FrameLayout {
+public final class QsbLayout extends FrameLayout {
 
-    ImageButton lensIcon;
-    AssistantIconView assistantIcon;
-    Context mContext;
+    private static final Intent SEARCH_INTENT = new Intent("android.search.action.GLOBAL_SEARCH")
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+    private static final Intent LENS_INTENT = Intent.makeMainActivity(
+        new ComponentName(
+            "com.google.ar.lens",
+            "com.google.vr.apps.ornament.app.lens.LensLauncherActivity"
+        )
+    ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+
+    private static final Intent VOICE_INTENT = new Intent(Intent.ACTION_VOICE_COMMAND)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+    private static final String QSB_PACKAGE = "com.google.android.googlequicksearchbox";
 
     public QsbLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext = context;
     }
 
     public QsbLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mContext = context;
     }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        assistantIcon = findViewById(R.id.mic_icon);
-        assistantIcon.setIcon();
-        lensIcon = findViewById(R.id.lens_icon);
-        String searchPackage = QsbContainerView.getSearchWidgetPackageName(mContext);
-        setOnClickListener(view -> {
-            mContext.startActivity(new Intent("android.search.action.GLOBAL_SEARCH").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK).setPackage(searchPackage));
-        });
-        if (searchPackage == "com.google.android.googlequicksearchbox") {
+        final String searchPackage = QsbContainerView.getSearchWidgetPackageName(getContext());
+        if (searchPackage == null) return;
+        setOnClickListener(v -> startActivity(SEARCH_INTENT.setPackage(searchPackage)));
+
+        final AssistantIconView assistantIcon = findViewById(R.id.mic_icon);
+        if (assistantIcon != null) {
+            assistantIcon.setIcon();
+            assistantIcon.setOnClickListener(v -> startActivity(VOICE_INTENT.setPackage(searchPackage)));
+        }
+
+        if (searchPackage.equals(QSB_PACKAGE)) {
             setupLensIcon();
         }
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int requestedWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
+    private void startActivity(final Intent intent) {
+        getContext().startActivity(intent);
+    }
 
-        DeviceProfile dp = ActivityContext.lookupContext(mContext).getDeviceProfile();
-        int cellWidth = DeviceProfile.calculateCellWidth(requestedWidth, dp.cellLayoutBorderSpacePx.x, dp.numShownHotseatIcons);
-        int iconSize = (int)(Math.round((dp.iconSizePx * 0.92f)));
-        int width = requestedWidth - (cellWidth - iconSize);
+    private void setupLensIcon() {
+        final ResolveInfo resolvedActivity = getContext().getPackageManager()
+            .resolveActivity(LENS_INTENT, ResolveInfoFlags.of(PackageManager.MATCH_ALL));
+        if (resolvedActivity == null) return;
+        final ImageButton lensIcon = findViewById(R.id.lens_icon);
+        if (lensIcon != null) {
+            lensIcon.setVisibility(View.VISIBLE);
+            lensIcon.setImageResource(R.drawable.ic_lens_color);
+            lensIcon.setOnClickListener(v -> startActivity(LENS_INTENT));
+        }
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        final int requestedWidth = MeasureSpec.getSize(widthMeasureSpec);
+        final int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        final DeviceProfile dp = ActivityContext.lookupContext(getContext()).getDeviceProfile();
+        final int cellWidth = DeviceProfile.calculateCellWidth(requestedWidth, dp.cellLayoutBorderSpacePx.x, dp.numShownHotseatIcons);
+        final int iconSize = (int) Math.round(dp.iconSizePx * 0.92f);
+        final int width = requestedWidth - (cellWidth - iconSize);
         setMeasuredDimension(width, height);
 
         for (int i = 0; i < getChildCount(); i++) {
@@ -61,20 +92,5 @@ public class QsbLayout extends FrameLayout {
                 measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
             }
         }
-
     }
-
-    private void setupLensIcon() {
-        Intent lensIntent = Intent.makeMainActivity(new ComponentName("com.google.ar.lens", "com.google.vr.apps.ornament.app.lens.LensLauncherActivity")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        if (getContext().getPackageManager().resolveActivity(lensIntent, 0) == null){
-            return;
-        }
-        lensIcon.setVisibility(View.VISIBLE);
-        lensIcon.setImageResource(R.drawable.ic_lens_color);
-
-        lensIcon.setOnClickListener(view -> {
-            mContext.startActivity(lensIntent);
-        });
-    }
-
 }
