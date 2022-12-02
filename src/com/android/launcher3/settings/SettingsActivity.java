7dab6f4a2dcdb02b66fb68bb7c33f16fba5afa0d
jhonboy121 <alfredmathew05@gmail.com>
2022-12-02 08:58:51 +0530

    Launcher3: Hide suggestions pref if unsupported
    Change-Id: I06412e2dd2bb48d798be4ef6e9a26a17b65277a7
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -25,8 +25,10 @@ import static com.android.launcher3.OverlayCallbackImpl.KEY_ENABLE_MINUS_ONE;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.ResolveInfoFlags;
+import android.content.pm.ResolveInfo;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -69,7 +71,7 @@ import java.util.List;
  */
 public class SettingsActivity extends CollapsingToolbarBaseActivity
         implements OnPreferenceStartFragmentCallback, OnPreferenceStartScreenCallback,
-        SharedPreferences.OnSharedPreferenceChangeListener{
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     /** List of fragments that can be hosted by this activity. */
     private static final List<String> VALID_PREFERENCE_FRAGMENTS = Collections.singletonList(
@@ -89,6 +91,10 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     static final String EXTRA_FRAGMENT = ":settings:fragment";
     @VisibleForTesting
     static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
+
+    private static final String KEY_SUGGESTIONS = "pref_suggestions";
+    private static final Intent SUGGESTIONS_INTENT =
+            new Intent("android.settings.ACTION_CONTENT_SUGGESTIONS_SETTINGS");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -295,6 +301,9 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                     mShowGoogleAppPref = preference;
                     updateIsGoogleAppEnabled();
                     return true;
+
+                case KEY_SUGGESTIONS:
+                    return areSuggestionsAvailable();
             }
 
             return true;
@@ -375,6 +384,15 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                             .performAccessibilityAction(ACTION_ACCESSIBILITY_FOCUS, null);
                 }
             });
+        }
+
+        private boolean areSuggestionsAvailable() {
+            final List<ResolveInfo> resolvedIntents = requireContext().getPackageManager()
+                .queryIntentActivities(
+                    SUGGESTIONS_INTENT,
+                    ResolveInfoFlags.of(PackageManager.MATCH_ALL)
+                );
+            return !resolvedIntents.isEmpty();
         }
     }
 }
