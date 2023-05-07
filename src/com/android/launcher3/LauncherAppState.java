c3d318f817104b6d631075d15310e810fc9e708e
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-07 22:32:18 +0700

    Launcher3: Use overridden IconProvider instance consistently
    Change-Id: If4f01b32bcde61a3a08e0630de25d05ef805fbb7
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -69,7 +69,7 @@ public class LauncherAppState implements SafeCloseable {
 
     private final Context mContext;
     private final LauncherModel mModel;
-    private final LauncherIconProvider mIconProvider;
+    private final IconProvider mIconProvider;
     private final IconCache mIconCache;
     private final InvariantDeviceProfile mInvariantDeviceProfile;
     private final RunnableList mOnTerminateCallback = new RunnableList();
@@ -152,7 +152,7 @@ public class LauncherAppState implements SafeCloseable {
         mContext = context;
 
         mInvariantDeviceProfile = InvariantDeviceProfile.INSTANCE.get(context);
-        mIconProvider = new LauncherIconProvider(context);
+        mIconProvider = IconProvider.INSTANCE.get(context);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile,
                 iconCacheFileName, mIconProvider);
         mModel = new LauncherModel(context, this, mIconCache, new AppFilter(mContext),
@@ -233,7 +233,10 @@ public class LauncherAppState implements SafeCloseable {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
             if (Themes.KEY_THEMED_ICONS.equals(key)) {
-                mIconProvider.setIconThemeSupported(Themes.isThemedIconEnabled(mContext));
+                if (mIconProvider instanceof LauncherIconProvider) {
+                    ((LauncherIconProvider) mIconProvider)
+                        .setIconThemeSupported(Themes.isThemedIconEnabled(mContext));
+                }
                 verifyIconChanged();
             }
         }
