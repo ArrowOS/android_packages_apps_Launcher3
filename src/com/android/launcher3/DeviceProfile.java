ArrowOS/android_packages_apps_Launcher3

commit cc2e8285c7c41f39cc2f3b4b479281b9ca1879c0

Launcher3: Introduce search bar in hotseat
This implementation utilizes the existing QsbContainerView which
automatically binds to the search app widget.

Squashed some fixes (co-authored):
- Launcher3: Restart launcher on google app change
- Launcher3: Improve spacing above and below dock searchbar
- Launcher3: Account hotseatBorderSpace into qsbWidth and
   hotseat padding

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
Change-Id: Id68e86a27c2934534ad97af6f994f1007724550f

src/com/android/launcher3/DeviceProfile.java
@@ -191,6 +191,7 @@ public class DeviceProfile {
     public final int hotseatQsbHeight;
     public final int hotseatQsbVisualHeight;
     private final int hotseatQsbShadowHeight;
+    private final int hotseatQsbExcessMargin;
     public int hotseatBorderSpace;
     private final int mMinHotseatIconSpacePx;
     private final int mMinHotseatQsbWidthPx;
@@ -451,9 +452,12 @@ public class DeviceProfile {
 
         workspaceCellPaddingXPx = res.getDimensionPixelSize(R.dimen.dynamic_grid_cell_padding_x);
 
-        hotseatQsbHeight = res.getDimensionPixelSize(R.dimen.qsb_widget_height);
+        boolean isQsbEnabled = Utilities.isHotseatQsbEnabled(context);
+        hotseatQsbHeight = isQsbEnabled ? res.getDimensionPixelSize(R.dimen.qsb_widget_height) : 0;
         hotseatQsbShadowHeight = res.getDimensionPixelSize(R.dimen.qsb_shadow_height);
         hotseatQsbVisualHeight = hotseatQsbHeight - 2 * hotseatQsbShadowHeight;
+        hotseatQsbExcessMargin = res.getDimensionPixelSize(
+                R.dimen.hotseat_qsb_excess_horizontal_margin);
 
         // Whether QSB might be inline in appropriate orientation (e.g. landscape).
         boolean canQsbInline = (isTwoPanels ? inv.inlineQsb[INDEX_TWO_PANEL_PORTRAIT]
@@ -472,6 +476,12 @@ public class DeviceProfile {
         int hotseatBarBottomSpace = pxFromDp(inv.hotseatBarBottomSpace[mTypeIndex], mMetrics);
         int minQsbMargin = res.getDimensionPixelSize(R.dimen.min_qsb_margin);
         hotseatQsbSpace = pxFromDp(inv.hotseatQsbSpace[mTypeIndex], mMetrics);
+
+        if (!isQsbEnabled) {
+            hotseatBarBottomSpace += minQsbMargin + hotseatQsbSpace;
+            hotseatQsbSpace = 0;
+        }
+
         // Have a little space between the inset and the QSB
         if (mInsets.bottom + minQsbMargin > hotseatBarBottomSpace) {
             int availableSpace = hotseatQsbSpace - (mInsets.bottom - hotseatBarBottomSpace);
@@ -663,12 +673,10 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
-        if (!isScalableGrid) return;
-
         int columns = inv.hotseatColumnSpan[mTypeIndex];
         float hotseatWidthPx = getIconToIconWidthForColumns(columns);
         hotseatBorderSpace = calculateHotseatBorderSpace(hotseatWidthPx, /* numExtraBorder= */ 0);
-        hotseatQsbWidth = calculateQsbWidth(hotseatBorderSpace);
+        hotseatQsbWidth = calculateQsbWidth(hotseatBorderSpace) + hotseatQsbExcessMargin;
         // Spaces should be correct when the nav buttons are not inline
         if (!areNavButtonsInline) {
             return;
@@ -1376,7 +1384,7 @@ public class DeviceProfile {
             // workspace cell vs a hotseat cell.
             float workspaceCellWidth = (float) widthPx / inv.numColumns;
             float hotseatCellWidth = (float) widthPx / numShownHotseatIcons;
-            int hotseatAdjustment = Math.round((workspaceCellWidth - hotseatCellWidth) / 2);
+            int hotseatAdjustment = hotseatBorderSpace / 2;
             hotseatBarPadding.set(
                     hotseatAdjustment + workspacePadding.left + cellLayoutPaddingPx.left
                             + mInsets.left,

res/layout/search_container_hotseat.xml
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!--
-     Copyright (C) 2021 The Android Open Source Project
+     Copyright (C) 2016 The Android Open Source Project
+               (C) 2023 ArrowOS
 
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
@@ -14,7 +15,17 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 -->
-<View
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="0dp" />
+<com.android.launcher3.qsb.QsbContainerView
+        xmlns:android="http://schemas.android.com/apk/res/android"
+        android:id="@id/search_container_hotseat"
+        android:orientation="vertical"
+        android:layout_width="match_parent"
+        android:layout_height="@dimen/qsb_widget_height"
+        android:padding="0dp">
+
+    <fragment
+        android:name="com.android.launcher3.qsb.QsbContainerView$QsbFragment"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:tag="qsb_view"/>
+</com.android.launcher3.qsb.QsbContainerView>

res/values/arrow_strings.xml
@@ -61,4 +61,8 @@
     <string name="laboratory_parallel_space_title">Parallel space</string>
     <string name="laboratory_parallel_space_summary">Allow running multiple instances of the same application in parallel</string>
 
+    <!-- Hotseat QSB -->
+    <string name="hotseat_qsb_pref_title">Show search bar</string>
+    <string name="hotseat_qsb_pref_summary">In the bottom dock</string>
+
 </resources>

res/values/config.xml
@@ -132,8 +132,10 @@
 
     <!-- Scalable Grid configuration -->
     <!-- This is a float because it is converted to dp later in DeviceProfile -->
-    <dimen name="hotseat_bar_bottom_space_default">48</dimen>
-    <dimen name="hotseat_qsb_space_default">0</dimen>
+    <dimen name="hotseat_bar_bottom_space_default">16</dimen>
+    <dimen name="hotseat_qsb_space_default">24</dimen>
+
+    <dimen name="hotseat_qsb_excess_horizontal_margin">8dp</dimen>
 
     <!-- Recents -->
     <item type="id" name="overview_panel"/>

res/values/dimens.xml
@@ -366,7 +366,7 @@
     <!-- Taskbar related (placeholders to compile in Launcher3 without Quickstep) -->
     <dimen name="taskbar_size">0dp</dimen>
     <dimen name="taskbar_stashed_size">0dp</dimen>
-    <dimen name="qsb_widget_height">0dp</dimen>
+    <dimen name="qsb_widget_height">64dp</dimen>
     <dimen name="qsb_shadow_height">0dp</dimen>
     <dimen name="min_hotseat_icon_space">18dp</dimen>
     <dimen name="max_hotseat_icon_space">50dp</dimen>

res/xml/launcher_preferences.xml
@@ -39,6 +39,13 @@
         android:title="@string/title_show_google_feed"/>
 
     <SwitchPreference
+        android:key="pref_hotseat_qsb"
+        android:title="@string/hotseat_qsb_pref_title"
+        android:summary="@string/hotseat_qsb_pref_summary"
+        android:defaultValue="true"
+        android:persistent="true"/>
+
+    <SwitchPreference
         android:defaultValue="false"
         android:key="pref_workspace_lock"
         android:persistent="true"

src/com/android/launcher3/Hotseat.java
@@ -27,6 +27,8 @@ import android.view.ViewDebug;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.android.launcher3.qsb.QsbContainerView;
+
 /**
  * View class that represents the bottom row of the home screen.
  */
@@ -41,6 +43,7 @@ public class Hotseat extends CellLayout implements Insettable {
     private boolean mSendTouchToWorkspace;
 
     private final View mQsb;
+    private final String mQsbPackageName;
 
     public Hotseat(Context context) {
         this(context, null);
@@ -54,7 +57,11 @@ public class Hotseat extends CellLayout implements Insettable {
         super(context, attrs, defStyle);
 
         mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
-        addView(mQsb);
+        mQsbPackageName = QsbContainerView.getSearchWidgetPackageName(context);
+
+        if (Utilities.isHotseatQsbEnabled(context)) {
+            addView(mQsb);
+        }
     }
 
     /**
@@ -199,4 +206,7 @@ public class Hotseat extends CellLayout implements Insettable {
         return mQsb;
     }
 
+    public String getQsbPackageName() {
+        return mQsbPackageName;
+    }
 }

src/com/android/launcher3/Utilities.java
@@ -77,6 +77,7 @@ import com.android.launcher3.icons.ThemedIconDrawable;
 import com.android.launcher3.model.data.ItemInfo;
 import com.android.launcher3.model.data.ItemInfoWithIcon;
 import com.android.launcher3.pm.ShortcutConfigActivityInfo;
+import com.android.launcher3.qsb.QsbContainerView;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.shortcuts.ShortcutRequest;
 import com.android.launcher3.testing.shared.ResourceUtils;
@@ -131,6 +132,9 @@ public final class Utilities {
     public static final int EDGE_NAV_BAR = 1 << 8;
 
     public static final String KEY_DT_GESTURE = "pref_dt_gesture";
+    public static final String KEY_HOTSEAT_QSB = "pref_hotseat_qsb";
+
+    public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -752,6 +756,23 @@ public final class Utilities {
     public static boolean isWorkspaceEditAllowed(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return !prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_LOCK, false);
+    }
+
+    public static boolean isQsbAvailable(Context context) {
+        return QsbContainerView.getSearchWidgetProviderInfo(context) != null;
+    }
+
+    public static boolean isHotseatQsbEnabledByUser(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_HOTSEAT_QSB, true);
+    }
+
+    public static boolean isHotseatQsbEnabled(Context context) {
+        return isQsbAvailable(context) && isHotseatQsbEnabledByUser(context);
+    }
+
+    public static void restart() {
+        Process.killProcess(Process.myPid());
     }
 
     /**

commit c8913b621535168169f54951170eb3772cd4f8a4

Launcher3: Skip recalculateHotseatWidthAndBorderSpace when QSB is disabled
Keep AOSP behaviour if QSB is disabled.

Fixes: cc2e8285c7 ("Launcher3: Introduce search bar in hotseat")
Change-Id: I1c00cb1666b78be907eb38a0886fe87229b35dd1

src/com/android/launcher3/DeviceProfile.java
@@ -576,7 +576,7 @@ public class DeviceProfile {
         mMaxHotseatIconSpacePx = areNavButtonsInline
                 ? res.getDimensionPixelSize(R.dimen.max_hotseat_icon_space) : Integer.MAX_VALUE;
         // Hotseat and QSB width depends on updated cellSize and workspace padding
-        recalculateHotseatWidthAndBorderSpace();
+        recalculateHotseatWidthAndBorderSpace(context);
 
         // AllApps height calculation depends on updated cellSize
         if (isTablet) {
@@ -672,7 +672,11 @@ public class DeviceProfile {
      * Calculates the width of the hotseat, changing spaces between the icons and removing icons if
      * necessary.
      */
-    public void recalculateHotseatWidthAndBorderSpace() {
+    public void recalculateHotseatWidthAndBorderSpace(Context context) {
+        if (!isScalableGrid && !Utilities.isHotseatQsbEnabled(context)) {
+            return;
+        }
+
         int columns = inv.hotseatColumnSpan[mTypeIndex];
         float hotseatWidthPx = getIconToIconWidthForColumns(columns);
         hotseatBorderSpace = calculateHotseatBorderSpace(hotseatWidthPx, /* numExtraBorder= */ 0);

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -460,7 +460,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
                 .filter(deviceProfile -> deviceProfile.isTablet)
                 .forEach(deviceProfile -> {
                     deviceProfile.numShownHotseatIcons = numMinShownHotseatIconsForTablet;
-                    deviceProfile.recalculateHotseatWidthAndBorderSpace();
+                    deviceProfile.recalculateHotseatWidthAndBorderSpace(context);
                 });
 
         ComponentName cn = new ComponentName(context.getPackageName(), getClass().getName());