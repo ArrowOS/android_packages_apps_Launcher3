28e8d4a528e7c601d5f71b15a98c92df547b3ae0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-18 21:29:44 +0530

    fixup! Launcher3: Re-organize home settings
    Handle preferences within categorites.
    Keep developer options at root.
    
    Change-Id: I037e12070cd3288c9a9100fe62a70de88f1e9e52

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -48,6 +48,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback;
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback;
@@ -232,7 +233,18 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             PreferenceScreen screen = getPreferenceScreen();
             for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
                 Preference preference = screen.getPreference(i);
-                if (initPreference(preference)) {
+                if (preference instanceof PreferenceCategory) {
+                    PreferenceCategory category = (PreferenceCategory) preference;
+                    for (int j = category.getPreferenceCount() - 1; j >= 0; j--) {
+                        Preference pref = category.getPreference(j);
+                        if (!initPreference(pref)) {
+                            category.removePreference(pref);
+                        }
+                    }
+                    if (category.getPreferenceCount() == 0) {
+                        screen.removePreference(category);
+                    }
+		} else if (initPreference(preference)) {
                     if (IS_STUDIO_BUILD && preference == mDeveloperOptionPref) {
                         preference.setOrder(0);
                     }
